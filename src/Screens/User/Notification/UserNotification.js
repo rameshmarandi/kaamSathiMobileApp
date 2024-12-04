d7a5f1a0282fd8716b9ed2b115ab3223f6e3cbe4
Ramesh Marandi <rameshmarandi@Rameshs-MacBook-Air.local>
2024-12-04 09:44:52 +0530

    delete notifiction functionality added

--- src/Screens/User/Notification/UserNotification.js
+++ src/Screens/User/Notification/UserNotification.js
@@ -6,11 +6,13 @@ import {
   Image,
   FlatList,
   TouchableOpacity,
+  SectionList,
+  RefreshControl,
 } from 'react-native';
 import {useSelector} from 'react-redux';
 import {getFontSize, getResHeight} from '../../../utility/responsive';
 import theme from '../../../utility/theme';
-import {StatusBarComp, trimText} from '../../../Components/commonComp';
+import {StatusBarComp} from '../../../Components/commonComp';
 import CustomHeader from '../../../Components/CustomHeader';
 import MsgConfig from '../../../Config/MsgConfig';
 import TabViewComp from '../../../Components/TabViewComp';
@@ -21,11 +23,10 @@ import {
   getNotificationAPIHander,
   readNotificationAPIHander,
 } from '../../../redux/reducer/Notification/NotificationAPI';
-import {RefreshControl} from 'react-native';
-import MasterDeleteSelect from '../../../Components/MasterDeleteSelect';
-import {getShortTimeAgo} from '../../../Components/commonHelper';
 import moment from 'moment';
-import {DateFormator} from '../../../Helpers/CommonHelpers';
+import MasterDeleteSelect from '../../../Components/MasterDeleteSelect';
+import ConfirmAlert from '../../../Components/ConfirmAlert';
+import {VectorIcon} from '../../../Components/VectorIcon';
 
 const UserNotification = ({navigation}) => {
   const {isDarkMode, currentBgColor, currentTextColor} = useSelector(
@@ -34,6 +35,8 @@ const UserNotification = ({navigation}) => {
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
   const [isLongPressed, setIsLongPressed] = useState(false);
   const [selectedIds, setSelectedIds] = useState([]); // Track selected notification IDs
+  const [isConfirmModalVisible, setIsConfirmModalVisible] = useState(false);
+  const [alertText, setAlertText] = useState(null);
   const {groupedNotifications} = useSelector(
     state => state.notification.getNotification,
   );
@@ -46,6 +49,21 @@ const UserNotification = ({navigation}) => {
     try {
       await store.dispatch(getNotificationAPIHander());
     } catch (error) {}
+  };
+  const getNotificationImage = eventType => {
+    const images = {
+      DailyVerse:
+        'https://cdn3d.iconscout.com/3d/premium/thumb/bible-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--christian-holy-book-miscellaneous-pack-illustrations-4122966.png',
+      Birthday:
+        'https://www.shutterstock.com/shutterstock/photos/2290920757/display_1500/stock-vector-vector-d-icon-party-popper-cartoon-emoji-of-birthday-confetti-explosion-simple-minimal-2290920757.jpg',
+      Anniversary:
+        'https://cdn3d.iconscout.com/3d/premium/thumb/anniversary-date-3d-icon-download-in-png-blend-fbx-gltf-file-formats--calendar-event-valentine-valentines-day-pack-festival-days-icons-6210503.png',
+      TransactionSuccess:
+        'https://media.istockphoto.com/id/639735440/vector/rupee-currency-icon-3d-rendering-illustration.jpg?s=612x612&w=0&k=20&c=fmu01_RFom-7N2ezxLPNi18z6lGyTLcqTUYtyb8iqmw=',
+      Default:
+        'https://cdn3d.iconscout.com/3d/premium/thumb/notifications-3d-icon-download-in-png-blend-fbx-gltf-file-formats--notification-bell-alarm-user-interface-pack-icons-5034125.png',
+    };
+    return images[eventType] || images.Default;
   };
 
   const notificationTabs = [
@@ -69,8 +87,22 @@ const UserNotification = ({navigation}) => {
     {key: 'sixth', title: 'Others', data: groupedNotifications?.Others},
   ];
 
+  const groupNotificationsByDate = notifications => {
+    if (!notifications || notifications.length === 0) return [];
+    const grouped = notifications.reduce((acc, notification) => {
+      const date = moment(notification.createdAt).format('DD-MMM-YYYY');
+      if (!acc[date]) acc[date] = [];
+      acc[date].push(notification);
+      return acc;
+    }, {});
+
+    return Object.keys(grouped).map(date => ({
+      title: date,
+      data: grouped[date],
+    }));
+  };
+
   const handleTabIndexChange = useCallback(index => {
-    console.log('get_curet', index);
     setCurrentTabIndex(index);
     setIsLongPressed(false);
     setSelectedIds([]);
@@ -79,10 +111,8 @@ const UserNotification = ({navigation}) => {
   const toggleCardSelection = id => {
     setSelectedIds(prevState => {
       if (prevState.includes(id)) {
-        // Deselect if already selected
         return prevState.filter(item => item !== id);
       } else {
-        // Select the card
         return [...prevState, id];
       }
     });
@@ -90,160 +120,148 @@ const UserNotification = ({navigation}) => {
 
   const isReadNotification = async id => {
     try {
-      const payload = {
-        notificationID: id,
-      };
-
+      const payload = {notificationID: id};
       await store.dispatch(readNotificationAPIHander(payload));
     } catch (error) {}
   };
-  const getNotificationImage = eventType => {
-    const images = {
-      DailyVerse:
-        'https://cdn3d.iconscout.com/3d/premium/thumb/bible-3d-illustration-download-in-png-blend-fbx-gltf-file-formats--christian-holy-book-miscellaneous-pack-illustrations-4122966.png',
-      Birthday:
-        'https://www.shutterstock.com/shutterstock/photos/2290920757/display_1500/stock-vector-vector-d-icon-party-popper-cartoon-emoji-of-birthday-confetti-explosion-simple-minimal-2290920757.jpg',
-      Anniversary:
-        'https://cdn3d.iconscout.com/3d/premium/thumb/anniversary-date-3d-icon-download-in-png-blend-fbx-gltf-file-formats--calendar-event-valentine-valentines-day-pack-festival-days-icons-6210503.png',
-      TransactionSuccess:
-        'https://media.istockphoto.com/id/639735440/vector/rupee-currency-icon-3d-rendering-illustration.jpg?s=612x612&w=0&k=20&c=fmu01_RFom-7N2ezxLPNi18z6lGyTLcqTUYtyb8iqmw=',
-      Default:
-        'https://cdn3d.iconscout.com/3d/premium/thumb/notifications-3d-icon-download-in-png-blend-fbx-gltf-file-formats--notification-bell-alarm-user-interface-pack-icons-5034125.png',
-    };
-    return images[eventType] || images.Default;
-  };
-
-  const renderNotificationCard = useCallback(
-    ({item}) => {
-      const notificationImage = getNotificationImage(item.eventType);
-      const isSelected = selectedIds.includes(item._id); // Check if this card is selected
-      return (
-        <TouchableOpacity
-          activeOpacity={0.8}
-          onLongPress={() => {
-            setIsLongPressed(true);
-            toggleCardSelection(item._id);
-          }} // Toggle selection on long press
-          onPress={() => {
-            if (isLongPressed) {
-              toggleCardSelection(item._id);
-            } else {
-              isReadNotification(item._id);
-            }
-          }} // Toggle selection on normal press
-          style={{
-            paddingVertical: '4%',
-            marginBottom: '2%',
-            paddingHorizontal: '5%',
-            marginHorizontal: '2%',
-            flexDirection: 'row',
-            borderBottomWidth: 0.3,
-            borderColor: 'grey',
-            borderRadius: getResHeight(2),
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexWrap: 'wrap',
-            backgroundColor: isSelected
-              ? 'rgba(255, 255, 255, 0.5)'
-              : !item.isRead
-              ? 'rgba(150, 149, 149, 0.199)'
-              : 'transparent', // Apply overlay on selection
-          }}>
-          <View>
-            <Image
-              source={{uri: notificationImage}}
-              style={{
-                height: getResHeight(6),
-                width: getResHeight(6),
-                backgroundColor: 'white',
-                borderRadius: getResHeight(100),
-                borderWidth: 0.2,
-                borderColor: currentTextColor,
-              }}
-            />
-          </View>
-          <View style={{marginLeft: '4%', flex: 1}}>
-            <Text
-              style={{
-                fontSize: getFontSize(1.5),
-                color: currentTextColor,
-                fontFamily: theme.font.medium,
-              }}>
-              {/* {trimText(item.message)} */}
-              {item.message}
-            </Text>
-            {/* <Text
-              style={{
-                fontSize: getFontSize(1.4),
-                color: currentTextColor,
-                fontFamily: theme.font.regular,
-              }}
-            /> */}
-            {/* <View>
-              <Text
-                style={{
-                  textDecorationLine: 'underline',
-                  color: currentTextColor,
-                }}>
-                Read more..
-              </Text>
-            </View> */}
-
-            <Text
-              style={{
-                color: currentTextColor,
-                fontFamily: theme.font.bold,
-                fontSize: getFontSize(1.5),
-                justifyContent: 'flex-end',
-                marginTop: '5%',
-              }}>
-              {`${DateFormator(
-                item.createdAt,
-                'DD-MMM-YYYY',
-              )}   (${getShortTimeAgo(moment(item.createdAt))})`}
-            </Text>
-          </View>
-        </TouchableOpacity>
-      );
-    },
-    [selectedIds, currentTextColor], // Re-render when selectedIds or currentTextColor change
-  );
-
-  const handleDeleteNotifications = async () => {
+  const deleteNotification = async () => {
     try {
-      const payload = {
-        notificationID: selectedIds,
-      };
-      await store.dispatch(deleteNotificationAPIHander(payload));
-      setIsLongPressed(false);
-      setSelectedIds([]);
+      await store.dispatch(
+        deleteNotificationAPIHander({
+          notificationID: selectedIds,
+        }),
+      );
     } catch (error) {}
   };
+  const renderNotificationCard = ({item}) => {
+    const notificationImage = getNotificationImage(item.eventType);
+    const isSelected = selectedIds.includes(item._id);
+
+    return (
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onLongPress={() => {
+          setIsLongPressed(true);
+          toggleCardSelection(item._id);
+        }}
+        onPress={() => {
+          if (isLongPressed) {
+            toggleCardSelection(item._id);
+          } else {
+            isReadNotification(item._id);
+          }
+        }}
+        style={{
+          paddingVertical: '4%',
+          marginBottom: '2%',
+          paddingHorizontal: '5%',
+          flexDirection: 'row',
+          borderBottomWidth: 0.3,
+          borderColor: 'grey',
+          backgroundColor: isSelected
+            ? 'rgba(255, 255, 255, 0.5)'
+            : !item.isRead
+            ? 'rgba(150, 149, 149, 0.199)'
+            : 'transparent',
+        }}>
+        <Image
+          source={{uri: notificationImage}}
+          style={{
+            height: getResHeight(6),
+            width: getResHeight(6),
+            borderRadius: getResHeight(100),
+            borderWidth: 0.2,
+            borderColor: currentTextColor,
+          }}
+        />
+        <View style={{marginLeft: '4%', flex: 1}}>
+          <Text
+            style={{
+              fontSize: getFontSize(1.5),
+              color: currentTextColor,
+              fontFamily: theme.font.medium,
+            }}>
+            {item.message}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  const renderSectionHeader = ({section: {title}}) => (
+    <View
+      style={{
+        backgroundColor: isDarkMode
+          ? 'rgba(255, 255, 255, 0.1)'
+          : 'rgba(0, 0, 0, 0.05)', // Subtle background based on mode
+        paddingVertical: getResHeight(1.5),
+        paddingHorizontal: '5%',
+        marginVertical: getResHeight(1),
+        borderRadius: getResHeight(1),
+        elevation: 1, // Light shadow for modern look
+        shadowColor: isDarkMode ? '#000' : '#ccc',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+      }}>
+      <Text
+        style={{
+          fontSize: getFontSize(1.8),
+          color: currentTextColor, // Adapt to the current theme's text color
+          fontFamily: theme.font.bold,
+          textAlign: 'center',
+          textTransform: 'uppercase', // Modern text styling
+          letterSpacing: 1.2, // Slight spacing for clean appearance
+        }}>
+        {title}
+      </Text>
+    </View>
+  );
 
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: currentBgColor}}>
       <StatusBarComp />
       <CustomHeader
-        backPress={() => {
-          navigation.goBack();
-        }}
+        backPress={() => navigation.goBack()}
         screenTitle={MsgConfig.notification}
-        onPressNotificaiton={() => {
-          // navigation.navigate('UserNotification');
-        }}
+        onPressNotificaiton={() => {}}
       />
-
-      {selectedIds.length > 0 && (
+      {isLongPressed && (
         <MasterDeleteSelect
           selectedItem={selectedIds}
           onClosePress={() => {
             setIsLongPressed(false);
+
             setSelectedIds([]);
-          }} // Reset selection when closing
-          onDeletePress={handleDeleteNotifications}
+          }}
+          onDeletePress={() => {
+            setAlertText(
+              `Are you sure you want to delete ${selectedIds.length} notifications?`,
+            );
+            setIsConfirmModalVisible(true);
+          }}
         />
       )}
 
+      <ConfirmAlert
+        visible={isConfirmModalVisible}
+        onCancel={() => setIsConfirmModalVisible(false)}
+        alertTitle={alertText}
+        alertIcon={
+          <VectorIcon
+            type={'MaterialIcons'}
+            name={'delete-forever'}
+            size={getFontSize(5.1)}
+            color={theme.color.error}
+          />
+        }
+        onConfirm={() => {
+          deleteNotification();
+          setIsConfirmModalVisible(false);
+          setSelectedIds([]);
+          setIsLongPressed(false);
+        }}
+      />
       <View style={{flex: 1, marginTop: '3%'}}>
         <TabViewComp
           routes={notificationTabs.map(({key, title}) => ({
@@ -252,41 +270,21 @@ const UserNotification = ({navigation}) => {
           }))}
           scenes={notificationTabs.reduce((acc, {key, data}) => {
             acc[key] = () => (
-              <FlatList
-                data={data}
-                contentContainerStyle={{
-                  flexGrow: 1,
-                  // marginTop: '5%',
-                  // paddingHorizontal: '5%',
-                }}
-                refreshControl={
-                  <RefreshControl
-                    refreshing={false}
-                    onRefresh={() => APICalling()}
-                  />
-                }
-                keyExtractor={(item, index) => item._id} // Use unique ID for keyExtractor
+              <SectionList
+                sections={groupNotificationsByDate(data)}
+                keyExtractor={item => item._id}
                 renderItem={renderNotificationCard}
+                renderSectionHeader={renderSectionHeader}
                 ListEmptyComponent={() => (
-                  <View
-                    style={{
-                      flex: 1,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      marginTop: getResHeight(-20),
-                    }}>
-                    <NoDataFound />
-                  </View>
+                  <NoDataFound style={{marginTop: getResHeight(-20)}} />
                 )}
+                refreshControl={
+                  <RefreshControl refreshing={false} onRefresh={APICalling} />
+                }
               />
             );
             return acc;
           }, {})}
-          indicatorStyle={{backgroundColor: 'red'}}
-          tabBarContainerStyle={{
-            backgroundColor: currentBgColor,
-            marginBottom: '4%',
-          }}
           onIndexChange={handleTabIndexChange}
           labelStyle={{
             color: currentTextColor,
